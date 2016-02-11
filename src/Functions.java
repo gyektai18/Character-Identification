53b66de16db23ccd1eb968592007b9bea8b29548
gyektai18 <gyektai18@deerfield.edu>
2016-02-11 13:30:05 -0500

    Make code neater. Neat code is good code.

---
+++ src/Functions.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
+
+public class Functions {
+	public void run() {
+		String csvFile = "/Users/gyektai18/Downloads/entityfacts.csv";
+		BufferedReader br = null;
+		String line = "";
+		String cvsSplitBy = ",";
+		Scanner scan = new Scanner(System.in);
+		try {
+
+			br = new BufferedReader(new FileReader(csvFile));
+			while ((line = br.readLine()) != null) {
+				System.out.println(line);
+				System.out.println("Hit enter for next line");
+				String test = scan.nextLine();
+			        // use comma as separator
+				String[] entity = line.split(cvsSplitBy);
+
+
+			}
+
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if (br != null) {
+				try {
+					br.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+
+		System.out.println("Done");
+	  }
+}
