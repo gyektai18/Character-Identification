9ed12441829a024a2db5cb7a14ec9b36e9615c27
gyektai18 <gyektai18@deerfield.edu>
2016-02-11 20:43:19 -0500

    Add another very quick comment just in case that you can't see the commit message that I did last time.

--- src/MainCallsAndSuch.java
+++ src/MainCallsAndSuch.java
@@ -4,6 +4,8 @@
  * I suggest running them separately
  * because they are unrelated in how they work.
  * Run the first two lines, then the second to do it neatly.
+ * I am submitting everything because Christine can't,
+ * and she said that she talked to you about that.
  */
 public class MainCallsAndSuch {
 	public static void main(String[] args){
